65dbfc93d46435d1af6ef82e61a556d47afc8791
TheSeeker07 <ryanameredith8@gmail.com>
2024-04-14 20:17:59 -0500

    Added script and form to tennessee

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-function validateForm(form) {
+(() => {
+    'use strict'
+    
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
+  function invalidEmails (a, b) {
 
-    if (form.InputEmail1.value != form.InputEmail2.value)
-    {
-        alert('Emails don\'t match!');
-        return false;
-    } else {
-        return true;
+    a = a.trim();
+    b = b.trim();
+    if (!a || !b) return "Missing email"; 
+    
+ 
+    const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    const enterEmail = re.test(a);
+    const confirmEmail = a === b;
+    
+    if      (!enterEmail) return "Invalid email";
+    else if (!confirmEmail) return "Emails do not match";
+  }
+  
+  function formSubmitHandler (evt) {
+    const is_emails_invalid = invalidEmails(this.enterEmail.value, this.confirmEmail.value);
+    if (is_emails_invalid) {
+      evt.preventDefault();      
+      alert(is_emails_invalid);  
     }
-}
+  }
+  
+  document.querySelector("#form").addEventListener("submit", formSubmitHandler);
